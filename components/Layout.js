33ee932a30e3caf9b99ca1e0100b9be3ab7103ba
Vseslav Rashchynski <vrashinskiyv@gmail.com>
2023-05-17 15:14:51 +0200

    feat: implement fetch request

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,12 @@
 import Head from "next/head"
-import styles from '../styles/Layout.module.css'
+import styles from '@/styles/Layout.module.css'
 import Header from './Header';
+import ShowCase from './Showcase';
 import Footer from "./Footer";
+import { useRouter } from "next/router";
 
 export default function Layout({title, keywords,description,children}){
+  const router = useRouter()
   return (
     <div>
       <Head>
@@ -11,7 +14,11 @@ export default function Layout({title, keywords,description,children}){
         <meta name="description" content={description}></meta>
         <meta name="keywords" content={keywords}></meta>
       </Head>
+
       <Header />
+
+      {router.pathname==='/' && <ShowCase />}
+
       <div className={styles.container}>
         {children}
       </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,26 @@
-import Link from "next/link";
 import Layout from "@/components/Layout";
+import { API_URL } from "../config";
 
-export default function Index() {
+export default function HomePage({events}) {
+
   return (
     <Layout>
-      <h1>Main Page</h1>
-      <Link href='/about'>About</Link>
+      <h1>Upcoming Events</h1>
+      {events.length===0 && <h3>No Events</h3>}
+
+      {events.map((evt)=> (
+        <h3 key={evt.id}>{evt.name}</h3>
+      ))}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
 }
